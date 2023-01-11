53fce689579fff4f8dffc9df56c212f4791eba2a
Christian Wu <christianwu56@gmail.com>
2023-01-11 14:28:35 -0800

    New conference

--- ghi/js/new-conference.js
+++ ghi/js/new-conference.js
@@ -1,28 +1,26 @@
 window.addEventListener('DOMContentLoaded', async () => {
 
-    const url = 'http://localhost:8000/api/states/';
+    const url = 'http://localhost:8000/api/locations/'
     const response = await fetch(url);
-
-    if (response.ok) {
+    if (response.ok)
+    {
         const data = await response.json();
-
-        const selectTag = document.getElementById('state');
-
-    for (let state of data.states) {
-
-        let option = document.createElement("option")
-        option.value = state.abbreviation;
-        option.innerHTML = state.name;
-        selectTag.appendChild(option);
+        const selectTag = document.getElementById('location')
+        for(let location of data.locations)
+        {
+            let option = document.createElement('option')
+            option.value = location.id
+            option.innerHTML = location.name
+            selectTag.appendChild(option)
         }
     }
 
-const formTag = document.getElementById('create-location-form');
+const formTag = document.getElementById('create-conference-form')
 formTag.addEventListener('submit', async event => {
     event.preventDefault();
     const formData = new FormData(formTag);
     const json = JSON.stringify(Object.fromEntries(formData));
-    const locationUrl = 'http://localhost:8000/api/locations/';
+    const locationUrl = 'http://localhost:8000/api/conferences/';
     const fetchConfig = {
         method: "post",
         body: json,
@@ -35,5 +33,5 @@ formTag.addEventListener('submit', async event => {
         formTag.reset();
         const newLocation = await response.json();
     }
-});
+})
 });
