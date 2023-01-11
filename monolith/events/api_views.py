5862d131d912d5ea7f2187615406ef5e4c8cc16d
Christian Wu <christianwu56@gmail.com>
2023-01-11 12:53:25 -0800

    new location

--- monolith/events/api_views.py
+++ monolith/events/api_views.py
@@ -228,3 +228,19 @@ def api_show_location(request, pk):
             encoder=LocationDetailEncoder,
             safe=False,
         )
+
+@require_http_methods(["GET"])
+def api_list_states(request):
+
+    states = State.objects.order_by('name')
+
+    state_list = []
+
+    for state in states:
+        states = {
+            "name": state.name,
+            "abbreviation": state.abbreviation
+        }
+        state_list.append(states)
+
+    return JsonResponse({"states": state_list})
