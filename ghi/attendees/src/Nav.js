377776e254fa4c39c1b7a6d7e88622328b405482
Christian Wu <christianwu56@gmail.com>
2023-01-17 22:33:16 -0800

    Finished

--- ghi/attendees/src/Nav.js
+++ ghi/attendees/src/Nav.js
@@ -1,37 +1,29 @@
-
-const Nav = () => {
+function Nav() {
     return (
-
-        <header>
-            <nav className="navbar navbar-expand-lg navbar-light bg-light">
-                <div className="container-fluid">
-                    <a className="navbar-brand" href="/">Conference GO!</a>
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
-                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                        <li className="nav-item">
-                            <a className="nav-link " aria-current="page" href="home.html">Home</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link  " aria-current="page" href="new-location.html">New location</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link  " aria-current="page" href="new-conference.html">New Conference</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link  " aria-current="page" href="new-presentation.html">New Presentation</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link  " aria-current="page" href="index.html">List of Attendees</a>
-                        </li>
-                    </ul>
-                    </div>
-                </div>
-            </nav>
-        </header>
-    )
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="/">Conference GO!</a>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <a className="nav-link" aria-current="page" href="/">Home</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" aria-current="page" href="new-location.html">New location</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" aria-current="page" href="new-conference.html">New conference</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" aria-current="page" href="new-presentation.html">New presentation</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+    );
 }
-
 export default Nav
